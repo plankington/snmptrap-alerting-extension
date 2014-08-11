a166567725653351d95aa316f37065fa62726aea
kunal.gupta <kunal.gupta@appdynamics.com>
2014-08-11 13:23:36 -0700

    Fixing path issues

--- src/main/custom/actions/send-snmp-trap/snmpTrapSender.bat
+++ src/main/custom/actions/send-snmp-trap/snmpTrapSender.bat
@@ -1,2 +1,2 @@
 @echo off
-java -Dlog4j.configuration=file:..\..\conf\log4j.xml -DSNMP_TRAP_SENDER_HOME=../.. -jar ..\..\lib\SNMPTrapSender.jar %*
+..\..\..\jdk\bin\java -Dlog4j.configuration=file:conf\log4j.xml -DSNMP_TRAP_SENDER_HOME=. -jar .\lib\SNMPTrapSender.jar %*
--- src/main/custom/actions/send-snmp-trap/snmpTrapSender.sh
+++ src/main/custom/actions/send-snmp-trap/snmpTrapSender.sh
@@ -17,4 +17,4 @@
 #
 ###
 
-java -Dlog4j.configuration=file:../../conf/log4j.xml -DSNMP_TRAP_SENDER_HOME=../.. -jar ../../lib/SNMPTrapSender.jar "$@"
+../../../jdk/bin/java -Dlog4j.configuration=file:conf/log4j.xml -DSNMP_TRAP_SENDER_HOME=. -jar ./lib/SNMPTrapSender.jar "$@"
